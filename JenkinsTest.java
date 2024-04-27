d96cfcb9af0101e077b2b5b7009797f554df61bb
KalinduWijerathna <115542246+KalinduWijerathna@users.noreply.github.com>
2024-04-27 19:07:50 +0530

    Create JenkinsTest.java

---
+++ JenkinsTest.java
@@ -0,0 +1,6 @@
+public class JenkinsTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+        System.out.println("This is a test Java program for Jenkins.");
+    }
+}
